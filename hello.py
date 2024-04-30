70a0f0c31c93f74b7b1cb135e942311a502bbc5f
Nathaniel Rodriguez <golansy0@gmail.com>
2024-04-29 20:52:41 -0700

    Templating example

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -8,5 +9,10 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/<name>")
-def hello(name):
+def hello_1(name):
     return f"Hello, {escape(name)}!"
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
